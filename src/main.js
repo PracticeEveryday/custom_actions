85715256452e981d85d8ae951b3a6f2715eecb6c
PracticeEveryday <developerforus@gmail.com>
2024-02-27 18:11:13 +0900

    feat: modify template

--- src/main.js
+++ src/main.js
@@ -7,20 +7,20 @@ const github = require('@actions/github')
  */
 async function run() {
     try {
-        // The `who-to-greet` input is defined in action metadata file
+        // `누구에게 인사할지` 입력은 액션 메타데이터 파일에 정의되어 있습니다.
         const whoToGreet = core.getInput('who-to-greet', { required: true })
         core.info(`Hello, ${whoToGreet}!`)
 
-        // Get the current time and set as an output
+        // 현재 시간을 가져와 출력으로 설정합니다.
         const time = new Date().toTimeString()
         core.setOutput('time', time)
 
-        // Output the payload for debugging
+        // 디버깅을 위한 페이로드 출력
         core.info(
             `The event payload: ${JSON.stringify(github.context.payload, null, 2)}`
         )
     } catch (error) {
-        // Fail the workflow step if an error occurs
+        // 오류가 발생하면 워크플로우 단계 실패
         core.setFailed(error.message)
     }
 }
