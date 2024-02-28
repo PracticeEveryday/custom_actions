cb2fd426b39c65d1a4de488fb17d5a17e1f3bcca
PracticeEveryday <developerforus@gmail.com>
2024-02-28 20:20:00 +0900

    feat: prettier code

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const github = require("@actions/github");
 const core = require("@actions/core");
 
 async function run() {
-    const {GITHUB_REPOSITORY, GITHUB_REF} = process.env;
+    const { GITHUB_REPOSITORY, GITHUB_REF } = process.env;
     const prNum = GITHUB_REF.match(/^refs\/pull\/(.+)\/merge$/)[1];
-    const [owner, repo] = GITHUB_REPOSITORY.split("/");
+    const [ owner, repo ] = GITHUB_REPOSITORY.split("/");
     const token = core.getInput("token");
-    const octokit = github.getOctokit(token)
+    const octokit = github.getOctokit(token);
 
     await octokit.rest.issues.createComment({
         owner,
